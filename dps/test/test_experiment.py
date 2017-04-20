1843c7921b830ea180a05c9d5761d1f1581bcbf0
e2crawfo <eric.crawford@mail.mcgill.ca>
2017-04-20 11:34:06 -0400

    Made tests specify seeds.

--- dps/test/test_experiment.py
+++ dps/test/test_experiment.py
@@ -5,9 +5,9 @@ from dps.experiments.pointer_following import train_pointer
 
 @pytest.mark.parametrize('config', ['default', 'rl', 'curriculum', 'rlcurriculum'])
 def test_simple_addition(config):
-    train_addition(log_dir='/tmp/dps/addition/', config=config)
+    train_addition(log_dir='/tmp/dps/addition/', config=config, seed=20)
 
 
 @pytest.mark.parametrize('config', ['default', 'rl', 'curriculum'])
 def test_pointer_following(config):
-    train_pointer(log_dir='/tmp/dps/pointer/', config='default')
+    train_pointer(log_dir='/tmp/dps/pointer/', config=config, seed=10)
