b9efd5bd23f12c0b46e10459dcadab10e3f51c7a
Eric Crawford <eric.crawford@mail.mcgill.ca>
2017-07-19 13:06:07 -0400

    Removed old file.

--- dps/rl/actor_critic.py
+++
@@ -1,4 +0,0 @@
-from dps import cfg
-from dps.utils import build_scheduled_value, Param, Parameterized
-from dps.rl import RLUpdater, Policy
-from dps.rl.value import NeuralValueEstimator, GeneralizedAdvantageEstimator
