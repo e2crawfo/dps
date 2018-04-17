0154db7e835b5e6ad18fa9c3fd32d25a9454cb12
Eric Crawford <eric.crawford@mail.mcgill.ca>
2018-04-17 12:41:48 -0700

    Updated cedar scripts.

--- scripts/nips_2018/exploration.py
+++ scripts/nips_2018/exploration.py
@@ -1,63 +1,43 @@
 import clify
-import numpy as np
 from dps.env.advanced import yolo_rl
 from dps.datasets import EMNIST_ObjectDetection
-from dps.train import PolynomialScheduleHook
 
 
 def prepare_func():
     from dps import cfg
-    stage0 = cfg.curriculum[0]
-    if cfg.initial_stage == "normal":
-        stage0.update(fixed_values=dict(obj=1))
-    elif cfg.initial_stage == "explore":
-        stage0.update(obj_exploration=1.0, obj_default=0.5)
-    else:
-        raise Exception()
-    stage0["area_weight"] = cfg.stage0_area_weight
+    cfg.kernel_size = (cfg.kernel_size, cfg.kernel_size)
 
 
 distributions = dict(
-    area_weight=list(np.linspace(0.1, 0.4, 3)),
-    stage0_area_weight=[.01, .025],
-    initial_stage=["normal", "explore"],
+    area_weight=list([0.25, 0.5, 1.0, 2.0]),
+    nonzero_weight=list([50, 100, 150, 200]),
     box_std=[0.0, 0.1],
+    kernel_size=[1, 3],
 )
 
-fragment = [
-    dict(obj_exploration=0.2,),
-    dict(obj_exploration=0.1,),
-    dict(obj_exploration=0.05,),
-]
-
-config = yolo_rl.small_test_config.copy(
+config = yolo_rl.good_sequential_config.copy(
     prepare_func=prepare_func,
-    patience=2500,
-    lr_schedule=1e-4,
     render_step=100000,
-    max_overlap=40,
-    n_val=16,
     eval_step=1000,
+    patience=10000,
+    area_weight=None,
+    nonzero_weight=None,
 
-    fixed_values=dict(),
-
-    curriculum=[
-        dict(fixed_weights="obj", nonzero_weight=0, patience=10000, max_steps=10000)
-    ],
-
-    hooks=[
-        PolynomialScheduleHook(
-            attr_name="nonzero_weight",
-            query_name="best_COST_reconstruction",
-            base_configs=fragment, tolerance=2,
-            initial_value=90, scale=5, power=1.0)
-    ]
+    # TODO
+    max_steps=2000,
 )
 
 # Create the datasets if necessary.
+print("Forcing creation of first dataset.")
 with config:
     train = EMNIST_ObjectDetection(n_examples=int(config.n_train), shuffle=True, example_range=(0.0, 0.9))
     val = EMNIST_ObjectDetection(n_examples=int(config.n_val), shuffle=True, example_range=(0.9, 1.))
+
+print("Forcing creation of second dataset.")
+with config.copy(config.curriculum[-1]):
+    train = EMNIST_ObjectDetection(n_examples=int(config.n_train), shuffle=True, example_range=(0.0, 0.9))
+    val = EMNIST_ObjectDetection(n_examples=int(config.n_val), shuffle=True, example_range=(0.9, 1.))
+
 
 from dps.hyper import build_and_submit
 clify.wrap_function(build_and_submit)(config=config, distributions=distributions)
--- scripts/nips_2018/run_exploration.sh
+++ scripts/nips_2018/run_exploration.sh
@@ -1,2 +1,6 @@
-# python exploration.py --name=exploration_short --n-repeats=1 --max-hosts=2 --ppn=12 --max-hosts=2 --cpp=2 --kind=slurm --wall-time=20mins --cleanup-time=2mins --slack-time=2mins --pmem=5000 --gpu-set=0,1,2,3 --ignore-gpu=False --error-on-timeout=False
-python exploration.py --name=exploration --n-repeats=1 --max-hosts=2 --ppn=12 --max-hosts=2 --cpp=2 --kind=slurm --wall-time=6hours --cleanup-time=30mins --slack-time=30mins --pmem=5000 --gpu-set=0,1,2,3 --ignore-gpu=False --error-on-timeout=False
+export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/gcc-5.4.0/lib64:$LD_LIBRARY_PATH
+python exploration.py --name=exploration_short --n-repeats=1 --max-hosts=1 --ppn=8 --max-hosts=1 --cpp=1 --kind=slurm --wall-time=20mins --cleanup-time=2mins --slack-time=2mins --pmem=7000 --gpu-set=0,1 --ignore-gpu=False --error-on-timeout=False --n-param-settings=8
+# python exploration.py --name=exploration_short --n-repeats=1 --max-hosts=1 --ppn=8 --max-hosts=1 --cpp=1 --kind=slurm --wall-time=20mins --cleanup-time=2mins --slack-time=2mins --pmem=5000 --gpu-set=0,1 --ignore-gpu=False --error-on-timeout=False --n-param-settings=8
+# python exploration.py --name=exploration_short --n-repeats=1 --max-hosts=1 --ppn=16 --max-hosts=1 --cpp=1 --kind=slurm --wall-time=20mins --cleanup-time=2mins --slack-time=2mins --pmem=5000 --gpu-set=0,1,2,3 --ignore-gpu=False --error-on-timeout=False --n-param-settings=16
+
+# python exploration.py --name=exploration --n-repeats=1 --max-hosts=2 --ppn=12 --max-hosts=2 --cpp=2 --kind=slurm --wall-time=6hours --cleanup-time=30mins --slack-time=30mins --pmem=5000 --gpu-set=0,1,2,3 --ignore-gpu=False --error-on-timeout=False
