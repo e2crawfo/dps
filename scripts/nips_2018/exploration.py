e4a837b1f4b81d0eeacd72137a896fa641e4c74e
Eric Crawford <eric.crawford@mail.mcgill.ca>
2018-04-20 09:26:32 -0700

    Update from cedar

--- scripts/nips_2018/exploration.py
+++ scripts/nips_2018/exploration.py
@@ -1,31 +1,28 @@
 import clify
+
+from dps import cfg
 from dps.env.advanced import yolo_rl
 from dps.datasets import EMNIST_ObjectDetection
 
 
-def prepare_func():
-    from dps import cfg
-    cfg.kernel_size = (cfg.kernel_size, cfg.kernel_size)
-
-
 distributions = dict(
-    area_weight=list([0.25, 0.5, 1.0, 2.0]),
-    nonzero_weight=list([50, 100, 150, 200]),
-    box_std=[0.0, 0.1],
-    kernel_size=[1, 3],
+    area_weight=list([1.5, 2.0, 2.5, 3.0]),
+    nonzero_weight=list([150, 200, 250, 300]),
 )
 
 config = yolo_rl.good_sequential_config.copy(
-    prepare_func=prepare_func,
     render_step=100000,
     eval_step=1000,
-    patience=10000,
+
+    max_experiences=175000,
+    patience=10000000,
+    max_steps=1000000,
+
     area_weight=None,
     nonzero_weight=None,
-
-    # TODO
-    max_steps=2000,
 )
+
+config.curriculum[-1]['max_experiences'] = 100000000
 
 # Create the datasets if necessary.
 print("Forcing creation of first dataset.")
@@ -35,9 +32,8 @@ with config:
 
 print("Forcing creation of second dataset.")
 with config.copy(config.curriculum[-1]):
-    train = EMNIST_ObjectDetection(n_examples=int(config.n_train), shuffle=True, example_range=(0.0, 0.9))
-    val = EMNIST_ObjectDetection(n_examples=int(config.n_val), shuffle=True, example_range=(0.9, 1.))
-
+    train = EMNIST_ObjectDetection(n_examples=int(cfg.n_train), shuffle=True, example_range=(0.0, 0.9))
+    val = EMNIST_ObjectDetection(n_examples=int(cfg.n_val), shuffle=True, example_range=(0.9, 1.))
 
 from dps.hyper import build_and_submit
 clify.wrap_function(build_and_submit)(config=config, distributions=distributions)
